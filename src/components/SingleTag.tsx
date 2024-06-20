cc8a7c1ed7f25ce241636b59f9b9e4ff98aef5a5
hazel <529246402@qq.com>
2024-06-21 00:50:57 +0800

    chore: change file name

--- src/components/SingleTag.tsx
+++ src/components/SingleTag.tsx
@@ -1,24 +1,31 @@
 import { defineComponent, ref } from 'vue'
 import RemoveComponent from './RemoveComponent'
 
-const TagCompName = 'Tag'
-const Tag = defineComponent({
+const TagCompName = 'SingleTag'
+const SingleTag = defineComponent({
   name: TagCompName,
   props: {
-    id: '',
-    name: '',
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
   },
   setup(props) {
     const tagRef = ref()
+    const name = ref(props.name)
     return () => (
       <span
         ref={tagRef}
         class="ml-1 bg-blue-500 text-white px-2 py-1 rounded"
       >
-        <span class="mr-1">{props.name}</span>
+        <span class="mr-1">{name.value}</span>
         <RemoveComponent />
       </span>
     )
   },
 })
-export default Tag
+export default SingleTag
