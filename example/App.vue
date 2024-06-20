eb2514f74b54490e16b07c665078b367c111aba5
hazel <529246402@qq.com>
2024-06-21 00:51:34 +0800

    fix: update example

--- example/App.vue
+++ example/App.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
-import { defineComponent, ref } from 'vue'
-import Tag from '../src/components/Tag'
+import { ref } from 'vue'
+import VueTags from '../src/components/VueTags'
 
-const tags = ref({
+const tags = ref([{
   name: 'test',
   id: '1',
-})
+}, {
+  name: 'test2',
+  id: '2',
+}],
+)
+function setTags(fn) {
+  tags.value = fn(tags.value)
+}
+function handleAddition(tag) {
+  setTags((prevTags) => {
+    return [...prevTags, tag]
+  })
+}
 </script>
 
 <template>
-  <div>
-    <h1>Vue Tags Example</h1>
-    <Tag
-      :id="tags.id"
-      :name="tags.name"
-    />
+  <div flex justify-center m-auto h-100vh w-100vw mt-10>
+    <div text-center>
+      <h1>Vue Tags Example</h1>
+      {{ tags }}
+      <VueTags
+        :tags="tags"
+        :handle-addition="handleAddition"
+      />
+    </div>
   </div>
 </template>
