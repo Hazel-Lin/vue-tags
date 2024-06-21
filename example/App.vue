92d479eea46bec664141e3975114197e240aed49
hazel <529246402@qq.com>
2024-06-22 00:17:00 +0800

    chore: update example

--- example/App.vue
+++ example/App.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import VueTags from '../src/components/VueTags'
+import type { Tag } from '../src/types'
 
-const tags = ref([{
-  name: 'test',
-  id: '1',
-}, {
-  name: 'test2',
-  id: '2',
-}],
+const tags = ref(
+  [
+    {
+      name: 'test',
+      id: '1',
+    },
+    {
+      name: 'test2',
+      id: '2',
+    },
+  ],
 )
-function setTags(fn) {
-  tags.value = fn(tags.value)
+function handleAddition(tag: Tag) {
+  tags.value = [...tags.value, tag]
 }
-function handleAddition(tag) {
-  setTags((prevTags) => {
-    return [...prevTags, tag]
-  })
+function handleDelete(index: number) {
+  tags.value = tags.value.filter((_, i) => i !== index)
+}
+function handleDeleteAll() {
+  tags.value = []
 }
 </script>
 
@@ -24,10 +30,12 @@ function handleAddition(tag) {
   <div flex justify-center m-auto h-100vh w-100vw mt-10>
     <div text-center>
       <h1>Vue Tags Example</h1>
-      {{ tags }}
       <VueTags
         :tags="tags"
         :handle-addition="handleAddition"
+        :handle-delete="handleDelete"
+        :handle-clear-all="handleDeleteAll"
+        allow-addition-from-paste
       />
     </div>
   </div>
