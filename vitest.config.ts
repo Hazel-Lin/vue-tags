c8db019811b2705c621142585f9d0b8dd5582e3f
linhuizi <529246402@qq.com>
2024-07-03 18:34:49 +0800

    chore: update deps about test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import VueJSX from '@vitejs/plugin-vue-jsx'
 
 const r = (p: string) => resolve(__dirname, p)
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VueJSX()],
   resolve: {
     alias: {
       '@': r('./src'),
@@ -19,7 +20,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     exclude: ['**/node_modules/**'],
-    include: ['./**/*.test.{ts,js}'],
+    include: ['./**/*.test.{ts,js,tsx}'],
     environmentOptions: {
       jsdom: {
         resources: 'usable',
