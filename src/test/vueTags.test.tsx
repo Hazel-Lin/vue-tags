599244fa2266211364a0adf932698e7d9b3beb6d
hazel <529246402@qq.com>
2024-07-10 01:05:14 +0800

    fix: update test file for handle

--- src/test/vueTags.test.tsx
+++ src/test/vueTags.test.tsx
@@ -2,13 +2,14 @@ import { describe, expect, it, vi } from 'vitest'
 import { fireEvent, render, screen } from '@testing-library/vue'
 import type { Tag } from '../types/vueTags'
 import VueTags from '../components/VueTags'
+import { CLEAR_ALL_TEXT, ENTER_ARROW_KEY, INPUT_TEST_ID, SINGLE_TAG_TEST_ID, TAG_DELETE_TEST_ID, TAG_EDIT_TEST_ID } from '@/common/constants'
+
+const sampleTags: Tag[] = [
+  { id: '1', name: 'Thailand' },
+  { id: '2', name: 'India' },
+]
 
 describe('test VueTags', () => {
-  const sampleTags: Tag[] = [
-    { id: '1', name: 'Thailand' },
-    { id: '2', name: 'India' },
-  ]
-
   it('should render correctly', () => {
     const { getByText } = render(VueTags, {
       props: {
@@ -20,10 +21,10 @@ describe('test VueTags', () => {
       expect(getByText(tag.name)).toBeTruthy()
     }
     // 输入框是否存在
-    expect(screen.getByRole('textbox')).toBeTruthy()
+    expect(screen.getByTestId(INPUT_TEST_ID)).toBeTruthy()
     expect(screen.getByPlaceholderText('Add new tag')).toBeTruthy()
     // 一键清空按钮是否存在
-    expect(getByText('一键清空')).toBeTruthy()
+    expect(getByText(CLEAR_ALL_TEXT)).toBeTruthy()
   })
 
   describe('when readOnly is true', () => {
@@ -35,7 +36,7 @@ describe('test VueTags', () => {
         },
       })
       // 当 readOnly 为 true 时，输入框不存在
-      expect(container.queryByTestId('input')).toBeNull()
+      expect(container.queryByTestId(INPUT_TEST_ID)).toBeNull()
     })
     it('should not edit tag', async () => {
       const container = render(VueTags, {
@@ -47,7 +48,7 @@ describe('test VueTags', () => {
       })
       // 点击某一个 tag 后，编辑框不存在
       await fireEvent.click(container.getByText('Thailand'))
-      expect(container.queryByTestId('tag-edit')).toBeNull()
+      expect(container.queryByTestId(TAG_EDIT_TEST_ID)).toBeNull()
     })
     it('should not delete tag', () => {
       const container = render(VueTags, {
@@ -57,7 +58,7 @@ describe('test VueTags', () => {
         },
       })
       // 删除按钮不存在
-      expect(container.queryByTestId('tag-delete')).toBeNull()
+      expect(container.queryByTestId(TAG_DELETE_TEST_ID)).toBeNull()
     })
     it('should not drag tag', async () => {
       const { getByText, container } = render(VueTags, {
@@ -67,6 +68,7 @@ describe('test VueTags', () => {
           allowDrag: true,
         },
       })
+      const list = [{ id: '1', name: 'Thailand' }, { id: '2', name: 'India' }]
       // 不存在这个样式
       expect(container.querySelectorAll('.cursor-move').length).toEqual(0)
       const dest = getByText('Thailand')
@@ -77,46 +79,33 @@ describe('test VueTags', () => {
       await fireEvent.dragOver(dest)
       await fireEvent.drop(dest)
       await fireEvent.dragEnd(dest)
+
+      expect(sampleTags).toEqual(list)
     })
   })
 
   describe('test handle', () => {
     // 测试回车可添加 tag
     it('should add a new tag when press enter', async () => {
+      const _sampleTags = [...sampleTags]
       const container = render(VueTags, {
         props: {
-          tags: sampleTags,
+          tags: _sampleTags,
         },
       })
       // 输入框输入 NewTag
-      const input = screen.getByTestId('input')
+      const input = screen.getByTestId(INPUT_TEST_ID)
       await fireEvent.change(input, { target: { value: 'test' } })
       // 按下 enter 键
-      await fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' })
+      await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
       // 判断是否添加成功
       expect(container.getByText('test')).toBeTruthy()
-      const list = [{
-        id: '1',
-        name: 'Thailand',
-      }, {
-        id: '2',
-        name: 'India',
-      }, {
-        id: '3',
-        name: 'test',
-      }]
-      expect(sampleTags).toEqual(list)
+      expect(_sampleTags).toEqual([...sampleTags, { id: '3', name: 'test' }])
     })
     // 测试删除 tag
     it('should delete a tag when click the remove button', async () => {
       const handleDelete = vi.fn()
-      const tags = [{
-        id: '1',
-        name: 'Thailand',
-      }, {
-        id: '2',
-        name: 'India',
-      }]
+      const tags = [...sampleTags]
       const container = render(VueTags, {
         props: {
           tags,
@@ -127,7 +116,7 @@ describe('test VueTags', () => {
         id: '2',
         name: 'India',
       }]
-      const deleteButtons = container.queryAllByTestId('tag-delete')
+      const deleteButtons = container.queryAllByTestId(TAG_DELETE_TEST_ID)
       await fireEvent.click(deleteButtons[0])
       expect(tags).toEqual(list)
       expect(handleDelete).toHaveBeenCalled()
@@ -143,10 +132,102 @@ describe('test VueTags', () => {
         },
       })
 
-      await fireEvent.click(container.getByText('一键清空'))
+      await fireEvent.click(container.getByText(CLEAR_ALL_TEXT))
 
       expect(handleClearAll).toHaveBeenCalled()
-      expect(container.queryAllByTestId('single-tag').length).toEqual(0)
+      expect(container.queryAllByTestId(SINGLE_TAG_TEST_ID).length).toEqual(0)
     })
+    // 测试粘贴函数是否生效
+    it('should paste tags when paste', async () => {
+      const _sampleTags = [...sampleTags]
+
+      render(VueTags, {
+        props: {
+          tags: _sampleTags,
+        },
+      })
+      const input = screen.getByTestId(INPUT_TEST_ID)
+      await fireEvent.paste(input, { clipboardData: { getData: () => 'hazel-lin' } })
+      expect(_sampleTags).toEqual([...sampleTags, { id: '3', name: 'hazel-lin' }])
+    })
+
+    it('should not paste tags when allowAdditionFromPaste is false', async () => {
+      const _sampleTags = [...sampleTags]
+      render(VueTags, {
+        props: {
+          tags: _sampleTags,
+          allowAdditionFromPaste: false,
+        },
+      })
+      const input = screen.getByTestId(INPUT_TEST_ID)
+      await fireEvent.paste(input, { clipboardData: { getData: () => 'hazel-lin' } })
+      expect(_sampleTags).toEqual(sampleTags)
+    })
+  })
+  it('should not add the same tag when the allowDuplicate is false', async () => {
+    const _sampleTags = [...sampleTags]
+    const count = sampleTags.length
+    const container = render(VueTags, {
+      props: {
+        tags: _sampleTags,
+        allowDuplicate: false,
+      },
+    })
+    const input = screen.getByTestId(INPUT_TEST_ID)
+    await fireEvent.change(input, { target: { value: 'hazel' } })
+    await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
+
+    expect(container.getByText('hazel')).toBeTruthy()
+    expect(_sampleTags.length).toEqual(count + 1)
+
+    await fireEvent.change(input, { target: { value: 'hazel' } })
+    await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
+
+    expect(_sampleTags.length).toEqual(count + 1)
+  })
+  it('should allow duplicate tags when allowDuplicate is true', async () => {
+    const _sampleTags = [...sampleTags]
+    const count = sampleTags.length
+    const container = render(VueTags, {
+      props: {
+        tags: _sampleTags,
+        allowDuplicate: true,
+      },
+    })
+    const input = screen.getByTestId(INPUT_TEST_ID)
+
+    await fireEvent.change(input, { target: { value: 'hazel' } })
+    await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
+
+    expect(container.getByText('hazel')).toBeTruthy()
+    expect(_sampleTags.length).toBe(count + 1)
+
+    await fireEvent.change(input, { target: { value: 'hazel' } })
+    await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
+
+    expect(_sampleTags).toEqual([...sampleTags, { id: '3', name: 'hazel' }, { id: '4', name: 'hazel' }])
+  })
+
+  it('should not add a new tag when the tags length is equal to maxTags', async () => {
+    const _sampleTags = [...sampleTags]
+    const maxTags = 3
+    const container = render(VueTags, {
+      props: {
+        tags: _sampleTags,
+        maxTags,
+      },
+    })
+    const input = screen.getByTestId(INPUT_TEST_ID)
+
+    await fireEvent.change(input, { target: { value: 'hazel' } })
+    await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
+
+    expect(container.getByText('hazel')).toBeTruthy()
+    expect(_sampleTags).toHaveLength(maxTags)
+
+    await fireEvent.change(input, { target: { value: 'lin' } })
+    await fireEvent.keyDown(input, { key: ENTER_ARROW_KEY, code: ENTER_ARROW_KEY })
+
+    expect(_sampleTags).toEqual([...sampleTags, { id: '3', name: 'hazel' }])
   })
 })
